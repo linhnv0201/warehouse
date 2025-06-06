f3188591023eef076f20fbf4c832629450225db9
Vu Linh Dev <linh.nv0201@gmail.com>
2025-06-06 16:31:40 +0700

    fix InventoryController

--- src/main/java/com/linh/warehouse/controller/InventoryController.java
+++ src/main/java/com/linh/warehouse/controller/InventoryController.java
@@ -1,5 +1,6 @@
 package com.linh.warehouse.controller;
 
+import com.linh.warehouse.dto.request.ApiResponse;
 import com.linh.warehouse.dto.response.InventoryResponse;
 import com.linh.warehouse.service.InventoryService;
 import lombok.RequiredArgsConstructor;
@@ -16,13 +17,19 @@ public class InventoryController {
     private final InventoryService inventoryService;
 
     @GetMapping
-    public ResponseEntity<List<InventoryResponse>> getAllInventories() {
-        return ResponseEntity.ok(inventoryService.getAllInventories());
+    public ApiResponse<List<InventoryResponse>> getAllInventories() {
+    return ApiResponse.<List<InventoryResponse>>builder()
+            .message("Lấy all inventories")
+            .result(inventoryService.getAllInventories())
+            .build();
+        //        return ResponseEntity.ok(inventoryService.getAllInventories());
     }
 
     @GetMapping("/warehouse/{warehouseId}")
-    public ResponseEntity<List<InventoryResponse>> getByWarehouseId(@PathVariable Integer warehouseId) {
-        return ResponseEntity.ok(inventoryService.getInventoriesByWarehouse(warehouseId));
-    }
+    public ApiResponse<List<InventoryResponse>> getByWarehouseId(@PathVariable Integer warehouseId) {
+        return ApiResponse.<List<InventoryResponse>>builder()
+                .message("Lấy all inventories của 1 warehouse")
+                .result(inventoryService.getInventoriesByWarehouse(warehouseId))
+                .build();    }
 }
 
